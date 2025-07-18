71c9ab1ce6bfbfd9426c20e78067f1bd2b7c382e
Karan Singh Parihar <karansingh9560796@gmail.com>
2025-07-18 21:25:06 +0530

    Commit

--- langgraph_logic.py
+++ langgraph_logic.py
@@ -1,4 +1,4 @@
-from langgraph.graph import StateGraph, END
+from langgraph import StateGraph, END
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 import os
